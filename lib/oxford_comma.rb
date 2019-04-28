a635182a6a289aa0f3959cef6bdab8ed52762934
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-28 15:42:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,6 @@ def oxford_comma(array)
    return "#{array[0]}"
    elsif array.length == 2 
    return array.join(" and ")
-   elsif array.length == 3
-   return array.join("  , and ")
+   
  end
 end
